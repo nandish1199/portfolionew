6b4475ab5c028013bb9b8588023943dd163f72b9
prettyworld3 <82945711+nandish1199@users.noreply.github.com>
2022-01-06 18:31:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggle() {
+  document.getElementById("btn1").style.display = "none";
+  document.getElementById("btn2").style.display = "block";
+  document.getElementById("navbarNav").style.display = "block";
+}
+function toggle2() {
+  document.getElementById("btn1").style.display = "block";
+  document.getElementById("btn2").style.display = "none";
+  document.getElementById("navbarNav").style.display = "none";
+}
